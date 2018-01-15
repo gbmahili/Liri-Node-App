648df624e015a196098ad3534ba3c19ee1b410ac
gbmahili <georgesmahili@gmail.com>
2018-01-15 12:15:23 -0500

    Installed the Twitter, Request, Spotify and Dotenv packages

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitter = {
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
